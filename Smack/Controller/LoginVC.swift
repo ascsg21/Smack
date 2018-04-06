7bcc12c8b96cbddc190b99f3a191d6e0a78471bd
MinRiDaddy <lovelyroom77@gmail.com>
2018-04-07 06:33:54 +0900

    4월 7일 commit

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by MinRiDaddy on 2018. 4. 7..
+//  Copyright © 2018년 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utillities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by MinRiDaddy on 2018. 4. 7..
+//  Copyright © 2018년 MinRiDaddy. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
