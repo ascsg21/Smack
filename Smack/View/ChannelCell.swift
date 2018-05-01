534137c3629b9ff613a821b8c34915d045a993d1
MinRiDaddy <lovelyroom77@gmail.com>
2018-05-01 16:21:41 +0900

    5월 1일 second commit

---
+++ Smack/View/ChannelCell.swift
@@ -0,0 +1,36 @@
+//
+//  ChannelCell.swift
+//  Smack
+//
+//  Created by MinRiDaddy on 2018. 5. 1..
+//  Copyright © 2018년 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var channelName: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    
+    func configureCell(channel : Channel) {
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+    }
+
+}
