aa6acb31dbcdec12d59eb18994f0290d6d8e0902
MinRiDaddy <lovelyroom77@gmail.com>
2018-05-02 21:11:36 +0900

    5월 2일 commit

---
+++ Smack/Services/SocketService.swift
@@ -0,0 +1,47 @@
+//
+//  SocketService.swift
+//  Smack
+//
+//  Created by MinRiDaddy on 2018. 5. 2..
+//  Copyright © 2018년 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+
+    static let instance = SocketService()
+    
+    override init() {
+        super.init()
+    }
+    
+    let socket : SocketIOClient = SocketManager(socketURL: URL(string: BASE_URL)!).defaultSocket
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func colseConnection() {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDesc = dataArray[1] as? String else { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(channelTitle: channelName, channelDescription: channelDesc, id: channelId)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+    
+}
